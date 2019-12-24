501b4d91f8cfb620e51becf21018145ace731383
shruti <shrutishastri49@gmail.com>
2019-12-25 00:13:12 +0530

    Added styles to task items

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -2,24 +2,44 @@ import React, { Component } from "react";
 
 import TodoList from "../components/TodoList";
 
-import DateTimePicker from "react-datetime-picker";
+//import DateTimePicker from "react-datetime-picker";
 import { Link } from "react-router-dom";
+
+import "./create.style.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 class CreateTodo extends Component {
   render() {
     const { input, date, todos } = this.props;
     return (
       <>
-        <form onSubmit={this.props.handleSubmit}>
-          <input type="text" onChange={this.props.createTodos} value={input} />
-          <DateTimePicker onChange={this.props.inputDate} value={date} />
-          <button>Add</button>
+        <form onSubmit={this.props.handleSubmit} className="todo-form">
+          <input
+            type="text"
+            onChange={this.props.createTodos}
+            value={input}
+            className="todo-input"
+            placeholder="Enter a todo.."
+          />
+          <button className="todo-btn">Add</button>
+          <TodoList todolist={todos} deleteTodo={this.props.deleteTodo} />
         </form>
-        <TodoList todolist={todos} deleteTodo={this.props.deleteTodo} />
-        <Link to="/searchTodos">Search</Link>
+
+        <Link to="/searchTodos" className="search-link">
+          <FontAwesomeIcon icon={faSearch} className="search-icon" />
+        </Link>
       </>
     );
   }
 }
 
 export default CreateTodo;
+{
+  /* <DateTimePicker
+            onChange={this.props.inputDate}
+            value={date}
+            className="todo-date"
+            /> */
+}
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+import "./list.style.css";
+
 class TodoList extends Component {
   getStyle = () => {
     return {
@@ -10,17 +15,19 @@ class TodoList extends Component {
   render() {
     const { todolist } = this.props;
     return (
-      <ul>
+      <ul className="todo-list">
         {todolist.map((list, index) => (
-          <li key={list.id} id={index}>
+          <li key={list.id} id={index} className="todo-item">
             {console.log(list.id)}
             <span>
-              <input type="checkbox" />
+              <input type="checkbox" className="input-check"/>
             </span>
             {list.title}
             <span>{list.date}</span>
             <span>
-              <button onClick={() => this.props.deleteTodo(list.id)}>X</button>
+              <button onClick={() => this.props.deleteTodo(list.id)} className="trash-btn">
+                <FontAwesomeIcon icon={faTrash} className="trash-icon"/>
+              </button>
             </span>
           </li>
         ))}
